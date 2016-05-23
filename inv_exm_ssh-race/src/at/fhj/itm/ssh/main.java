580488600ac5b7a5102dc06d7739d352db1edf82
Stousn <stefan.reip@edu.fh-joanneum.at>
2016-05-23 11:36:18 +0200

    Java project with simple hello world added

---
+++ inv_exm_ssh-race/src/at/fhj/itm/ssh/main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package at.fhj.itm.ssh;
+
+/**
+ * @author Helmuth Weithaler, Simon Schrei, Stefan Reip
+ *
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Seawas Wöd!");
+
+	}
+
+}
