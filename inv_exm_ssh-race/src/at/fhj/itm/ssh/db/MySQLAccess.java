95f18ff578a77d238ff53b2c45356372ef18513b
Stousn <stefan.reip@edu.fh-joanneum.at>
2016-07-12 16:50:31 +0200

    MySQLAccess reads DB-Properties from db.properties

--- inv_exm_ssh-race/src/at/fhj/itm/ssh/db/MySQLAccess.java
+++ inv_exm_ssh-race/src/at/fhj/itm/ssh/db/MySQLAccess.java
@@ -2,24 +2,59 @@ package at.fhj.itm.ssh.db;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class MySQLAccess {
 	private Connection connect = null;
 	private Statement statement = null;
 	private PreparedStatement preparedStatement = null;
 	private ResultSet resultSet = null;
+	private static final String DB_PROPFILE = "db.properties";
+
+	private static String db_username 		= "";
+	private static String db_password 		= "";
+	private static String db_url			= "";
+	private static String db_database 		= "";
+
+	public static void setProperties() {
+		Properties props = new Properties();
+		try(InputStream in = MySQLAccess.class.getClassLoader().getResourceAsStream(DB_PROPFILE))
+		{
+			props.load(in);
+			in.close();
+
+			db_username = props.getProperty("db.user");
+			db_password = props.getProperty("db.password");
+			db_url = props.getProperty("jdbc.url");
+			db_database = props.getProperty("db.database");
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+			System.err.println(DB_PROPFILE + " nicht gefunden");
+			System.exit(1);
+		}
+	}
 
 	public MySQLAccess() {
+		setProperties();
 		try {
+			//DEBUG INFO: db.properties: 
+//			System.out.println("DB.URL: "+db_url);
+//			System.out.println("DB.DB: "+db_database);
+//			System.out.println("DB.USER: "+db_username);
+//			System.out.println("DB.PW: "+db_password);
 			connect = DriverManager
-					.getConnection("jdbc:mysql://localhost:3306/inverted_exam?"
-							+ "user=root");
+					.getConnection(db_url+db_database+"?user="+db_username+"&password="+db_password);
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -34,6 +69,12 @@ public class MySQLAccess {
 			e.printStackTrace();
 		}
 	}
+	
+	public Connection getConnection() {
+		return this.connect;
+	}
+	
+	
 
 	public void readDataBase() throws Exception {
 		try {
@@ -49,7 +90,7 @@ public class MySQLAccess {
 			statement = connect.createStatement();
 			// Result set get the result of the SQL query
 			resultSet = statement
-					.executeQuery("select * from test");
+					.executeQuery("select * from BRAND");
 			writeResultSet(resultSet);
 
 
@@ -68,16 +109,16 @@ public class MySQLAccess {
 			// also possible to get the columns via the column number
 			// which starts at 1
 			// e.g. resultSet.getSTring(2);
-			int i1 = resultSet.getInt("a");
-			int i2 = resultSet.getInt("b");
-			int i3 = resultSet.getInt("c");
-			int i4 = resultSet.getInt("d");
-			int i5 = resultSet.getInt("e");
+			int i1 = resultSet.getInt("ID");
+			String i2 = resultSet.getString("CODE");
+			String i3 = resultSet.getString("NAME");
+//			int i4 = resultSet.getInt("d");
+//			int i5 = resultSet.getInt("e");
 			System.out.println("i1: " + i1);
 			System.out.println("i2: " + i2);
 			System.out.println("i3: " + i3);
-			System.out.println("i4: " + i4);
-			System.out.println("i5: " + i5);
+//			System.out.println("i4: " + i4);
+//			System.out.println("i5: " + i5);
 		}
 	}
 
